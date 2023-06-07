58247e0e6bd81db857f4f2fb24824ed44b04d4a9
Ilkin Mengusoglu <imengusoglu@gmail.com>
2023-06-07 23:41:04 +0100

    feat: output and input now on same window

--- gui_simplex/input_gui.py
+++ gui_simplex/input_gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tableau import Tableau
 
 LABEL = """Overwrite the example to input your linear program
 
@@ -7,16 +8,18 @@ Note: '+2 +1 == 10' for '2*x + y = 10'
 EXAMPLE = r"""Max: +2 +3 +1
 +1 +1 +1 <= 40
 +2 +1 -1 >= 10
-+0 -1 +1 == 10
++0 -1 +1 >= 10
 """
 
-class Input(tk.Tk):
+class Simplex(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.label = tk.Label(self, text=LABEL, font=("Courier", 18))
-        self.label.pack(padx=20, pady=20, fill="both")
+        self.output = None
 
-        self.text = tk.Text(self, font=("Courier", 18), height=4)
+        self.in_label = tk.Label(self, text=LABEL, font=("Courier", 18))
+        self.in_label.pack(padx=20, pady=20, fill="both")
+
+        self.text = tk.Text(self, font=("Courier", 18), height=6)
         self.text.pack()
         self.text.insert("1.0", EXAMPLE)
 
@@ -27,20 +30,26 @@ class Input(tk.Tk):
             height=1,
             width=10,
             text="Commit",
-            command=lambda: [self.retrieve_input(), self.destroy()],
+            command=lambda: self.simplex()
         )
         self.button_commit.pack()
 
-    def retrieve_input(self):
+        self.bind("<Return>", self.simplex)
+
+    def simplex(self):
         self.input_str = self.text.get("1.0", "end-1c")
-
-
-class Output(tk.Tk):
-    def __init__(self, d):
-        super().__init__()
-        self.label_text = self.make_str(d)
-        self.label = tk.Label(self, text=self.label_text, font=("Courier", 18))
-        self.label.pack(padx=20, pady=20, fill="both")
+        constraints = self.input_str.strip().splitlines()
+        constraints, n_art_vars = Tableau.preprocess(constraints)
+        t = Tableau(constraints, n_art_vars)
+        t.create_tableau(constraints)
+        t.generate_col_titles()
+        t.delete_empty()
+        try:
+            t.run_simp()
+        except Exception:
+            self.out_text = self.make_str(t.output_dict)
+            self.out_label = tk.Label(self, text=self.out_text, font=("Courier", 18))
+            self.out_label.pack(padx=20, pady=20, fill="both")
 
     @staticmethod
     def make_str(d):
@@ -48,3 +57,8 @@ class Output(tk.Tk):
         for i, j in d.items():
             string +=  f"\n {i} = " + str(j)
         return string
+
+
+if __name__ == "__main__":
+    simp = Simplex()
+    simp.mainloop()
--- gui_simplex/main.py
+++ gui_simplex/main.py
@@ -22,8 +22,5 @@ def main():
         out.mainloop()
 
 
-
-
-
 if __name__ == "__main__":
     main()
